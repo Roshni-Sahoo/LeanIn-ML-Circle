373fbb1f98810f8faafe82263aa3decd2c5f9528
Roshni Sahoo <roshni.sahoo19@gmail.com>
2021-04-14 12:51:08 +0530

    Python Libraries assignment completed

---
+++ python-libraries-assignment/Ques1_Array_of_10_zeros.py
@@ -0,0 +1,13 @@
+import numpy as np
+arr=np.zeros(10)
+print("Array of 10 0s=")
+print(arr)
+arr=np.ones(10)
+print("Array of 10 1s=")
+print(arr)
+arr=np.ones(10)*5
+print("Array of 10 5s")
+print(arr)
+arr=np.arange(30,70)
+print("Array from 30 to 70")
+print(arr)
---
+++ python-libraries-assignment/ques5_random_array.py
@@ -0,0 +1,5 @@
+import numpy as np
+x=np.random.rand(10,4)
+print(x)
+y=x[:5,:]
+print(y)
